79e7598754a2a742b4715f5b53bb090231f48555
wkd-woo <wkdwoos@gmail.com>
2021-10-28 23:05:34 +0900

    Google 0Auth

--- src/main/java/com/wkdwoo/book/springboot/web/IndexController.java
+++ src/main/java/com/wkdwoo/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.wkdwoo.book.springboot.web;
 
+import com.wkdwoo.book.springboot.config.auth.dto.SessionUser;
 import com.wkdwoo.book.springboot.domain.posts.PostsRepository;
 import com.wkdwoo.book.springboot.service.posts.PostsService;
 import com.wkdwoo.book.springboot.web.dto.PostsResponseDto;
@@ -9,17 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
         // 여기서는 postsService.findAllDesc()를 가져온 결과를 posts로 index.mustache에 전달
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
